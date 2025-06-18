db1edcd28dc8e4ba495d4d44cd6689a77b4e3795
MARCOOOOOOOOOOOOOOOOO123 <162756460+MARCOOOOOOOOOOOOOOOOO123@users.noreply.github.com>
2025-06-18 11:13:48 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,9 +12,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body>
-        <NavBar/>
+        
         {children} 
-        <Footer /> 
+        
       </body>
     </html>
   );
