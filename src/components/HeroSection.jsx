d285db8117d0a426e43f02b3ed127cbbb3cb1488
MARCOOOOOOOOOOOOOOOOO123 <162756460+MARCOOOOOOOOOOOOOOOOO123@users.noreply.github.com>
2025-06-18 13:28:43 +0100

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,19 +8,16 @@ const HeroSection = () => {
   return (
     <section className={styles.heroSection}>
       {/* Première div : Conteneur du texte et du bouton à gauche */}
-      <div className={styles.contentContainer}> {/* Ajout de la classe manquante */}
+      <div className={styles.contentContainer}>
         <h1 className={styles.heroTitle}>
-          Transformez vos clics en clients
-        </h1>
-        <h1 className={styles.heroTitle}>
-          avec nos campagnes Facebook
-        </h1>
-        <h1 className={styles.heroTitle}>
+          Transformez vos clics en clients <br />
+          avec nos campagnes Facebook <br />
           Ads sur-mesure
         </h1>
         
         <p className={styles.heroSubtitle}>
-          Des campagnes Facebook ciblées, des leads qualifiés livrés prêts à convertir.
+          Des campagnes Facebook ciblées, des leads qualifiés livrés <br /> 
+          prêts à convertir.
         </p>
         <Link href="/prendre-rdv" className={styles.heroButton}>
           Prendre RDV
@@ -29,7 +26,7 @@ const HeroSection = () => {
       {/* Deuxième div : Conteneur de l'image à droite */}
       <div className={styles.imageContainer}>
         <Image
-          src="/images/image_banniere.png" // Assure-toi que cette image est dans public/images/
+          src="/images/image banniere (1).png" // Assurez-vous que cette image est bien dans public/images/
           alt="Illustration de la génération de leads avec Facebook Ads"
           width={400} // Ajuste ces valeurs pour être responsive, basé sur le design
           height={600} // Ajuste ces valeurs pour être responsive, basé sur le design
