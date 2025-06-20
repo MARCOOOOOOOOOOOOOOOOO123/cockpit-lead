a6a62a5f6fd0e9e996af40dc67070cd5ce390693
MARCOOOOOOOOOOOOOOOOO123 <162756460+MARCOOOOOOOOOOOOOOOOO123@users.noreply.github.com>
2025-06-20 10:19:22 +0100

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,30 +8,28 @@ const HeroSection = () => {
   return (
     <section className={styles.heroSection}>
       {/* Première div : Conteneur du texte et du bouton à gauche */}
-      <div className={styles.contentContainer}> {/* Ajout de la classe manquante */}
+      <div className={styles.contentContainer}>
         <h1 className={styles.heroTitle}>
-          Transformez vos clics en clients
+          GÉNÉREZ DES LEADS QUALIFIÉS GRÂCE <br />À NOS
+          CAMPAGNES FACEBOOK ADS PERSONNALISÉES, CONÇUES
+          POUR <br />CONVERTIR VOS PROSPECTS EN CLIENTS.
         </h1>
-        <h1 className={styles.heroTitle}>
-          avec nos campagnes Facebook
-        </h1>
-        <h1 className={styles.heroTitle}>
-          Ads sur-mesure
-        </h1>
-        
-        <p className={styles.heroSubtitle}>
-          Des campagnes Facebook ciblées, des leads qualifiés livrés prêts à convertir.
-        </p>
-        <Link href="/prendre-rdv" className={styles.heroButton}>
+
+           <Link 
+          href="https://calendly.com/agylstudio1/30min" // URL Calendly mise à jour avec l'email
+          target="_blank" // Ouvre dans un nouvel onglet
+          rel="noopener noreferrer" // Bonne pratique de sécurité
+          className={styles.heroButton}
+        >
           Prendre RDV
         </Link>
       </div>
       {/* Deuxième div : Conteneur de l'image à droite */}
       <div className={styles.imageContainer}>
         <Image
-          src="/images/image_banniere.png" // Assure-toi que cette image est dans public/images/
+          src="/images/image banniere (1).png" // Assurez-vous que cette image est bien dans public/images/
           alt="Illustration de la génération de leads avec Facebook Ads"
-          width={400} // Ajuste ces valeurs pour être responsive, basé sur le design
+          width={600} // Ajuste ces valeurs pour être responsive, basé sur le design
           height={600} // Ajuste ces valeurs pour être responsive, basé sur le design
           priority // Important pour l'image LCP de la page
           className={styles.heroImage}
